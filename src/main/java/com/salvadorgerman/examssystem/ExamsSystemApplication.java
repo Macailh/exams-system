cc163c2e5c20058aa099bd756af07f9ebbe14773
Saen German <salvadorenriquegb@gmail.com>
2023-01-06 07:11:24 -0600

    Initial commit

---
+++ src/main/java/com/salvadorgerman/examssystem/ExamsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.salvadorgerman.examssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamsSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salvadorgerman/examssystem/ExamsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvadorgerman.examssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
