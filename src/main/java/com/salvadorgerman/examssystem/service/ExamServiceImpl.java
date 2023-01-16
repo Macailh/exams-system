2beb5c911bd292bf56e929ac655e57974e5c4eb6
Saen German <salvadorenriquegb@gmail.com>
2023-01-16 09:07:29 -0600

    Add mote endpoints and methods for exams

--- src/main/java/com/salvadorgerman/examssystem/service/ExamServiceImpl.java
+++ src/main/java/com/salvadorgerman/examssystem/service/ExamServiceImpl.java
@@ -1,10 +1,12 @@
 package com.salvadorgerman.examssystem.service;
 
+import com.salvadorgerman.examssystem.persistence.entity.Category;
 import com.salvadorgerman.examssystem.persistence.entity.Exam;
 import com.salvadorgerman.examssystem.persistence.repository.ExamRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -42,5 +44,20 @@ public class ExamServiceImpl implements ExamService{
         exam.setId(id);
         examRepository.delete(exam);
 
+    }
+
+    @Override
+    public List<Exam> getExamnsFromCategory(Category category) {
+        return examRepository.findByCategory(category);
+    }
+
+    @Override
+    public List<Exam> getActiveExams() {
+        return examRepository.findByActve(true);
+    }
+
+    @Override
+    public List<Exam> getActiveExamsFromCategory(Category category) {
+        return examRepository.findByCategoryAndActve(category, true);
     }
 }
